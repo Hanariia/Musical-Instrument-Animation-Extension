628aa715bbdd5c9331b2de8ee3b941c873b056a7
Hana Drtílková <xdrtilk@fi.muni.cz>
2025-02-02 17:01:09 +0100

    :bug: added handling of `tmp/hand_poses` dir

--- hand_pose_image_manager.py
+++ hand_pose_image_manager.py
@@ -1,4 +1,5 @@
 import json
+import os
 import subprocess
 from dataclasses import dataclass
 from math import floor
@@ -7,7 +8,7 @@ from typing import Tuple, List, Optional
 from .estimated_hand_poses import EstimatedHandPoses, HandPose, HandType
 
 
-HAND_POSES_DIRECTORY = "tmp/hand_poses/"
+HAND_POSES_DIRECTORY = "tmp/hand_poses"
 
 
 @dataclass
@@ -27,6 +28,11 @@ class HandPoseImageManager:
     def __init__(self, image_size: Tuple[int, int], hand_poses_filepath: str):
         self.image_size: Tuple[int, int] = image_size  # height, width
         self.estimated_hand_poses: EstimatedHandPoses = EstimatedHandPoses(filepath=hand_poses_filepath)
+        if os.path.isdir(HAND_POSES_DIRECTORY):
+            for file in os.listdir(HAND_POSES_DIRECTORY):
+                os.remove(os.path.join(HAND_POSES_DIRECTORY, file))
+        else:
+            os.makedirs(HAND_POSES_DIRECTORY)
 
     def get_frames_image_strip_data(self, frame, fps, previous_frames_count, next_frames_count):
         # todo: implement adjacent frames processing, maybe use queue instead of list?
@@ -56,7 +62,7 @@ class HandPoseImageManager:
         if hand_pose.image_filename is None:
             filename = f"hand_pose{hand_pose.index}{hand_pose.hand_type.name}.png"
             subprocess.run(['python', "draw_handmarks.py",
-                            HAND_POSES_DIRECTORY + filename,
+                            os.path.join(HAND_POSES_DIRECTORY, filename),
                             str(self.image_size[0]),
                             str(self.image_size[1]),
                             json.dumps(hand_pose.normalized_positions)])
