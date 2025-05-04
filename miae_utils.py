522e643200b2ac45c649ea36723583c9e1e1f763
xdrtilk@fi.muni.cz <xdrtilk@fi.muni.cz>
2025-05-04 17:23:41 +0200

    :lipstick: improved Preview setup + :bug: fixed Preview setup bug when in 'Animation' layout.

--- miae_utils.py
+++ miae_utils.py
@@ -1,9 +1,10 @@
 import os
+from typing import Optional
 
 import bpy
 
 
-def find_area(context, area_type: str) -> bpy.types.Area:
+def find_area(context, area_type: str) -> Optional[bpy.types.Area]:
     for area in context.screen.areas:
         if area.type == area_type:
             return area
